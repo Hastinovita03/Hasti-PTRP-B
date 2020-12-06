82c74b71d48a0e7eae78e895cbd5ecb9fb317923
Hastinovita03 <75230842+Hastinovita03@users.noreply.github.com>
2020-12-07 01:15:20 +0700

    Add files via upload

---
+++ TitikAwal_1.js
@@ -0,0 +1,8 @@
+var json_TitikAwal_1 = {
+"type": "FeatureCollection",
+"name": "TitikAwal_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "Point", "coordinates": [ 110.427522402743364, -7.073556625056832 ] } }
+]
+}
